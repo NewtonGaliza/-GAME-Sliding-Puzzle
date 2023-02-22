e55274d4e95e033d9973a20b583772e64f00e3d3
Newton Galiza <newtonjgaliza@gmail.com>
2023-02-22 12:13:10 -0300

    puzzle and number box
    puzzle and number box

---
+++ 15-puzzle/Assets/Scripts/NumberBox.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberBox : MonoBehaviour
+{
+    [SerializeField] int index = 0;
+    private int x = 0;
+    private int y = 0;
+
+    private Action<int, int> swapFunc = null;
+
+    public void Init(int i, int j, int index, Sprite sprite, Action<int, int> swapFunc)
+    {
+        this.index = index;
+        this.GetComponent<SpriteRenderer>().sprite = sprite;
+        UpdatePos(i,j);
+        this.swapFunc = swapFunc;
+    }
+
+    public void UpdatePos(int i, int j)
+    {
+        x = i;
+        y = j;
+        this.gameObject.transform.localPosition = new Vector2(i,j);
+    }
+
+    public bool IsEmpty()
+    {
+        return index == 16;
+    }
+
+    void OnMouseDown()
+    {
+        if(Input.GetMouseButtonDown(0) && swapFunc != null)
+        {
+            swapFunc(x,y);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
